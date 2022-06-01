4a9144ac4fa72fff82fb5d9e012208793211b642
Pimenta <mateuspimartins@hotmail.com>
2022-06-01 01:58:05 -0300

    getGames started

---
+++ routes/gamesRouter.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { createGame, getGames } from "../controllers/gameController.js";
+import { validateGame } from "../middlewares/gameValidator.js";
+
+const gamesRouter = Router();
+
+gamesRouter.get("/games", getGames);
+gamesRouter.post("/games", validateGame, createGame);
+
+export default gamesRouter;
