ccdcd31097d818f94518359e84a99763d30a7202
Pimenta <mateuspimartins@hotmail.com>
2022-06-02 01:35:25 -0300

    post and put customer

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -43,3 +43,56 @@ export async function getCustomer(req, res) {
     res.sendStatus(500)
   }
 }
+
+export async function createCustomer(req, res) {
+  const customer = req.body
+  try {
+    const result = await db.query('SELECT id FROM customers WHERE cpf = $1', [customer.cpf])
+    if (result.rowCount > 0) {
+      return res.sendStatus(409)
+    }
+
+    await db.query(`
+      INSERT INTO customers (name, phone, cpf, birthday) 
+      VALUES ($1, $2, $3, $4);
+    `, [customer.name, customer.phone, customer.cpf, customer.birthday])
+
+    res.sendStatus(201)
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(500)
+  }
+}
+
+export async function updateCustomer(req, res) {
+  const customer = req.body
+  const { id } = req.params
+
+  if (isNaN(parseInt(id))) {
+    return res.sendStatus(400)
+  }
+
+  try {
+    const result = await db.query(`
+      SELECT id FROM customers WHERE cpf = $1 AND id != $2
+    `, [customer.cpf, id])
+    if (result.rowCount > 0) {
+      return res.sendStatus(409)
+    }
+
+    await db.query(`
+      UPDATE customers 
+      SET 
+        name = $1, 
+        phone = $2, 
+        cpf = $3, 
+        birthday = $4 
+      WHERE id = $5
+    `, [customer.name, customer.phone, customer.cpf, customer.birthday, id])
+
+    res.sendStatus(200)
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(500)
+  }
+}
