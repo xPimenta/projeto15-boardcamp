12d92b4ad2dd0c2432a314f6dbf4fefccae91f05
Pimenta <mateuspimartins@hotmail.com>
2022-06-02 02:01:14 -0300

    rentals OK

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,13 @@ import { Router } from "express"
 import categoryRouter from "./categoryRouter.js"
 import gamesRouter from "./gamesRouter.js"
 import customersRouter from "./customersRouter.js"
+import rentalsRouter from "./rentalsRouter.js"
 
 
 const router = Router()
 router.use(categoryRouter)
 router.use(gamesRouter)
 router.use(customersRouter)
-
+router.use(rentalsRouter)
 
 export default router
